74feb64433a6065e6885bdeeeba77361a6136252
T0biii <admin@T0biii.de>
2020-03-01 12:51:37 +0100

    Update File

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ FFMUC_REPO=https://github.com/T0biii/site-ffm.git
 FFMUC_VERSION=T0biii-test-for-docker
 
 git clone $FFMUC_REPO site-ffm
-cd site-ffm 
+cd site-ffm
 git checkout -b patched && git checkout $FFMUC_VERSION
 make
